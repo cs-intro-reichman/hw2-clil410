a38f2fc3cc1574b4839fe36bfe6041ebffb2d0e8
Clil Koren <clilkoren@h-mhsb-nyyd-sl-Clil.local>
2024-11-15 12:51:19 +0200

    fixedhomework2

--- Cheers.java
+++ Cheers.java
@@ -14,14 +14,11 @@ public class Cheers {
                 } else {
                         a = "a";
                 }
-                System.out.println("Give me " + a + " " + word.charAt(i) + ": " + word.charAt(i) + "!" );
+                System.out.println("Give me " + a + " " + Character.toUpperCase(word.charAt(i)) + ": " +Character.toUpperCase(word.charAt(i)) + "!" );
             } 
             System.out.println("What does that spell?");
             for (int i=0; i < N; i++) {
-                System.out.println(word + "!!!");
+                System.out.println(word.toUpperCase() + "!!!");
             }
         }
-
-
 }
-
